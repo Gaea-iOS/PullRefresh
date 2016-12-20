3ca6e96c309c0c598fff6bd2dca8400c6cc08bd7
wangxiaotao <445242970@qq.com>
2016-12-20 22:18:29 +0800

    init commit

--- Example/PullRefresh/ViewController.swift
+++ Example/PullRefresh/ViewController.swift
@@ -7,18 +7,103 @@
 //
 
 import UIKit
+import PullRefresh
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    private var onceToken = UUID().uuidString
+    
+    private var _count: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.tableFooterView = UIView()
+        
+//        automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.addPullRefresh {
+            print("pull refreshing action!!!")
+            
+            let time = DispatchTime.now() + Double(Int64(1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: time, execute: {
+                self.tableView.stopPullRefresh()
+                self._count = 20
+                self.tableView.reloadData()
+            })
+        }
+        
+        tableView.addPushRefresh {
+            print("push refreshing action!!!")
+            
+            let time = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: time, execute: {
+                self.tableView.stopPushRefresh()
+                self._count += 20
+                self.tableView.reloadData()
+            })
+        }
+        
+//        let _ = delay(0.3) {
+//            self.tableView.startPullRefresh()
+//        }
     }
-
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+//        Once(token: onceToken).execute {
+//            tableView.contentInset = UIEdgeInsets(top: topLayoutGuide.length, left: 0, bottom: bottomLayoutGuide.length, right: 0)
+//            tableView.scrollIndicatorInsets = tableView.contentInset
+//            tableView.contentOffset.y = -topLayoutGuide.length
+//        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return _count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+    
+    @IBAction func clickLeftButton(sender: AnyObject) {
+        
+        //        tableView.startPullRefresh()
+        //        var scrollViewInsets = tableView.contentInset
+        //        scrollViewInsets.bottom += 50
+        //        UIView.animate(withDuration: 0.5, animations: {
+        //            self.tableView.contentInset = scrollViewInsets
+        //            self.tableView.contentOffset.y = -14
+        //
+        //        }, completion: { _ in
+        //
+        //        })
+        //        _count = 30
+        //        tableView.reloadData()
+        tableView.removePullRefresh()
+    }
+    
+    @IBAction func clickRightButton(sender: AnyObject) {
+        //        let contentInset = UIEdgeInsets(top: topLayoutGuide.length, left: 0, bottom: bottomLayoutGuide.length, right: 0)
+        //        UIView.animate(withDuration: 0.5, animations: {
+        //            self.tableView.contentInset = contentInset
+        //        }, completion: { _ in
+        //
+        //        })
+        //        tableView.stopPullRefresh()
+        //        _count = 0
+        //        tableView.reloadData()
+        tableView.removePushRefresh()
+    }
 }
 
---
+++ PullRefresh/Classes/PushRefreshableContainer.swift
@@ -0,0 +1,158 @@
+//
+//  PushRefreshableContainer.swift
+//  Gaea-Example
+//
+//  Created by 王小涛 on 2016/12/18.
+//  Copyright © 2016年 王小涛. All rights reserved.
+//
+
+import UIKit
+
+class PushRefreshableContainer: UIView {
+    
+    enum PullToRefreshState {
+        case pulling
+        case triggered
+        case refreshing
+        case stoped
+    }
+    
+    fileprivate let contentOffsetKeyPath = "contentOffset"
+    fileprivate let contentSizeKeyPath = "contentSize"
+    fileprivate var kvoContext = "pullToRefreshViewKVOContext"
+    
+    let refreshView: RefreshViewType
+    fileprivate var scrollViewInsets: UIEdgeInsets = .zero
+    fileprivate let refreshAction: ((Void) -> Void)?
+    
+    var state: PullToRefreshState = .stoped {
+        didSet {
+            guard state != oldValue else { return }
+            switch state {
+            case .pulling:
+                break;
+            case .triggered:
+                break;
+            case .refreshing:
+                startAnimation()
+            case .stoped:
+                stopAnimation()
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(frame: CGRect, refreshView: RefreshViewType, refreshAction: ((Void) -> Void)? = nil) {
+        
+        self.refreshView = refreshView
+        self.refreshAction = refreshAction
+        
+        super.init(frame: frame)
+        addSubview(refreshView.refreshView)
+        isHidden = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        refreshView.refreshView.frame = bounds
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        unregister()
+        if let scrollView = newSuperview as? UIScrollView {
+            scrollView.addObserver(self, forKeyPath: contentOffsetKeyPath, options: [.initial, .new], context: &kvoContext)
+            scrollView.addObserver(self, forKeyPath: contentSizeKeyPath, options: [.initial, .new], context: &kvoContext)
+        }
+    }
+    
+    deinit {
+        print("\(self) deinit")
+        unregister()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        guard context == &kvoContext,
+            keyPath == contentOffsetKeyPath || keyPath == contentSizeKeyPath,
+            let scrollView = object as? UIScrollView else {
+                super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+                return
+        }
+
+        guard state != .refreshing else { return }
+        
+        frame.origin.y = max(scrollView.contentSize.height, scrollView.bounds.size.height - scrollView.contentInset.top - scrollView.contentInset.bottom)
+        
+        let progress = (scrollView.contentOffset.y + scrollView.frame.size.height - scrollView.contentInset.bottom - frame.origin.y) / frame.size.height
+        
+//        print("push progress = \(progress)")
+        
+        guard progress > 0 else {
+            isHidden = true
+            return
+        }
+        
+        isHidden = false
+        
+        if scrollView.isDragging {
+            refreshView.pulling(progress: progress)
+            state = .pulling
+        }
+        
+        guard [.pulling, .triggered].contains(state) else { return }
+        
+        if progress >= 1 {
+            state = scrollView.isDragging ? .triggered : .refreshing
+        }else {
+            state = .pulling
+        }
+    }
+}
+
+private extension PushRefreshableContainer {
+    
+    func unregister() {
+        if let scrollView = superview as? UIScrollView {
+            scrollView.removeObserver(self, forKeyPath: contentOffsetKeyPath, context: &kvoContext)
+            scrollView.removeObserver(self, forKeyPath: contentSizeKeyPath, context: &kvoContext)
+        }
+    }
+    
+    func startAnimation() {
+        
+        guard let scrollView = superview as? UIScrollView else { return }
+        
+        isHidden = false
+        
+        refreshView.startRefreshAnimation()
+        
+        scrollViewInsets = scrollView.contentInset
+        
+        var insets = scrollViewInsets
+        insets.bottom += frame.size.height
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            scrollView.contentInset = insets
+        }, completion: { _ in
+            self.refreshAction?()
+        })
+    }
+    
+    func stopAnimation() {
+        
+        guard let scrollView = superview as? UIScrollView else { return }
+        
+        refreshView.stopRefreshAnimation()
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            scrollView.contentInset = self.scrollViewInsets
+        }, completion: { _ in
+            self.isHidden = true
+        })
+    }
+}
+
+
